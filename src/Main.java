eaaecd51a329d0ae57a9d7ede07e1c8249b744e1
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-15 15:30:32 +0100

    this is the constant section

--- src/Main.java
+++ src/Main.java
@@ -4,27 +4,9 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // 1 dim
+        // Constants
+        final float PI = 3.14f;
 
-//        int[] nums = new int[5];
-
-//        Arrays.sort(nums);
-//        System.out.println(nums.length);
-//        System.out.println(Arrays.toString(nums));
-
-        //  2 dim
-//        int[][] nums = new int[2][3];
-//        int[][] nums = {{1,2,3},{4,5,6}};
-
-//
-//        nums[1][2] = 20;
-//
-//        System.out.println(Arrays.deepToString(nums));
-
-        // 3 dim
-//        int[][][] nums = new int[2][3][4];
-        int[][][] nums = {{{1, 2, 3}, {4, 5, 6}}, {{1, 2, 3}, {4, 5, 6}}};
-        System.out.println(Arrays.deepToString(nums));
     }
 
 
