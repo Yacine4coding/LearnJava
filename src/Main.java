47a5d4b70e0a15253335d08f85679f8ac8cb51c7
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-15 15:26:46 +0100

    this is the array version of java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,30 @@
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-//        Primitive types
-        byte age = 30;
-        long viewsCount = 3_123_456_789L;
-        float price = 10.99F;
-        char letter = 'A';
-        boolean isElgble = false;
-//        System.out.println(age);
+        // 1 dim
 
-//         Reference types
+//        int[] nums = new int[5];
 
-        Date now = new Date();
-//        System.out.println(now);
-        //    Primitive types vs  Reference types
+//        Arrays.sort(nums);
+//        System.out.println(nums.length);
+//        System.out.println(Arrays.toString(nums));
 
-        byte x = 1;
-        byte y = x;
-        x = 2;// didn't effect Y
-//        System.out.println(y);
+        //  2 dim
+//        int[][] nums = new int[2][3];
+//        int[][] nums = {{1,2,3},{4,5,6}};
 
-        Point point1 = new Point(1,1);
-        Point point2 = point1;
-        point1.x = 2; // did effect point2
-//        System.out.println(point2);
+//
+//        nums[1][2] = 20;
+//
+//        System.out.println(Arrays.deepToString(nums));
 
-//        Strings
-
-        String message = "c:\\Windows\\...";
-
-//        System.out.println(message.replace("😎🙂",""));
-        System.out.println(message);   // didn't effect the original msg
+        // 3 dim
+//        int[][][] nums = new int[2][3][4];
+        int[][][] nums = {{{1, 2, 3}, {4, 5, 6}}, {{1, 2, 3}, {4, 5, 6}}};
+        System.out.println(Arrays.deepToString(nums));
     }
 
 
