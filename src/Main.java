648c5e5f3baf7c862f69d4a5074489d116a5b00b
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-15 15:40:40 +0100

    arthm expression in java

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,20 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
-        // Constants
-        final float PI = 3.14f;
+        // Arthm. expression
+        int rslt = 10 - 5; // 5
+
+        double test = 10 / 3d;
+
+        test++;
+        int x = 5;
+        int y = x++; // y = 5 ; x = 6
+        int y = ++x; // y = 6 ; x = 6
+
+        x += 2; // x = x + 2
+
+        System.out.println(test);
+
 
     }
 
