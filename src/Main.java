1438d63eaed646c82db6c195dc3b2a30834e7f9d
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-18 11:39:22 +0100

    this is the conditionel part

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,57 @@
-
-import java.text.NumberFormat;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        avoid magic numbers
-        final byte MONTHS_IN_YEAR = 12;
-        final byte PERCENT = 100;
+//        comparison op (same as js)
+//        int x = 1;
+//        int y = 1;
+//        System.out.println(x == y);
+
+//        Logic op
+//        int temp = 10;
+//        boolean isWarm = temp > 20 && temp < 30;
+//        System.out.println(isWarm);
+
+//        boolean hasHightIncom = true;
+//        boolean hasGoodCredit = true;
+//        boolean hasCriminalRecord = false;
+//        boolean isEligible = (hasGoodCredit || hasHightIncom) && !hasCriminalRecord;
+//        System.out.println(isEligible);
+
+//        if statments
+//        if(temp > 30){
+//            System.out.println("It's a hot day \nDrink plenty of water");
+//        }
+//        else if(temp > 20)
+//            System.out.println("It's a nice day");
+//        else
+//            System.out.println("it's cold");
+
+//        Simplify if statement
+
+//        int income = 120_000;
+//        boolean hasHighIncome = (income > 100_000);
+//        System.out.println(hasHighIncome);
+
+//        Ternary operation
+//        String className = income > 100_000? "first" : "Economy";
+//        switch statement
+//        String role = "admin";
+//        switch (role){
+//            case "director":
+//                System.out.println("You are an director");
+//                break;
+//            case "admin":
+//                System.out.println("You are an admin");
+//                break;
+//            case "moderator":
+//                System.out.println("You are a moderator");
+//                break;
+//            default:
+//                System.out.println("you are a guest!");
+//        }
 
 
-//        Get the Principle
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Princple:  ");
-        int princple = scanner.nextLong();
 
-//        get the annual interest Rate
-        System.out.print("Annual Interest Rate:  ");
-        float annualRate = scanner.nextFloat();
-
-//        get the period (year)
-        System.out.print("Period (Year):  ");
-        byte periodYo = scanner.nextByte();
-
-//        set n & r
-        int n = periodYo * MONTHS_IN_YEAR;
-        float r = annualRate / PERCENT / MONTHS_IN_YEAR;
-
-//        calculate the equation
-        double resutl = princple * ( (r*Math.pow((1+r),n)) / (Math.pow((1+r),n)-1));
-
-//        Print the result as Currency
-        String mrtg = NumberFormat.getCurrencyInstance().format(resutl);
-        System.out.println("Mortgage : " + mrtg);
     }
-
-
 }
