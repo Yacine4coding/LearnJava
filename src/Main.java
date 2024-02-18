8959dc1e76fc97b77007af2ab7a07a48fd56e8fe
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-18 10:22:55 +0100

    this is the section of reading inputs

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,16 @@ import java.text.NumberFormat;
 import java.text.ParsePosition;
 import java.util.Arrays;
 import java.util.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        Math class
-        int resutl1 = Math.round(1.1F);
-        int resutl2 = (int)Math.ceil(1.1F);
-        int resutl3 = Math.max(5,8);
-        int resutl4 = (int) (Math.random()*100);
-//        Formating Nums
-        NumberFormat numberFormat = NumberFormat.getCurrencyInstance();
-        NumberFormat percentInstance = NumberFormat.getPercentInstance();
-        String rslt = percentInstance.format(0.5);
-        System.out.println(rslt);
-//        shift + f6 to change variable name
+//  Reading input
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Name : ");
+//        byte age = scanner.nextByte();
+        String text = scanner.nextLine().trim().toUpperCase();
+        System.out.println("you are " +  text);
     }
 
 
