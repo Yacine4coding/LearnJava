1e9656ca1ab9146e2142d2c0bea1bd557b557450
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-18 11:05:32 +0100

    Mortgage Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,37 @@
-import java.awt.*;
-import java.text.FieldPosition;
+
 import java.text.NumberFormat;
-import java.text.ParsePosition;
-import java.util.Arrays;
-import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//  Reading input
+//        avoid magic numbers
+        final byte MONTHS_IN_YEAR = 12;
+        final byte PERCENT = 100;
+
+
+//        Get the Principle
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Name : ");
-//        byte age = scanner.nextByte();
-        String text = scanner.nextLine().trim().toUpperCase();
-        System.out.println("you are " +  text);
+        System.out.print("Princple:  ");
+        int princple = scanner.nextLong();
+
+//        get the annual interest Rate
+        System.out.print("Annual Interest Rate:  ");
+        float annualRate = scanner.nextFloat();
+
+//        get the period (year)
+        System.out.print("Period (Year):  ");
+        byte periodYo = scanner.nextByte();
+
+//        set n & r
+        int n = periodYo * MONTHS_IN_YEAR;
+        float r = annualRate / PERCENT / MONTHS_IN_YEAR;
+
+//        calculate the equation
+        double resutl = princple * ( (r*Math.pow((1+r),n)) / (Math.pow((1+r),n)-1));
+
+//        Print the result as Currency
+        String mrtg = NumberFormat.getCurrencyInstance().format(resutl);
+        System.out.println("Mortgage : " + mrtg);
     }
 
 
