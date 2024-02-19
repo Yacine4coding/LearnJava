af7565e8c7709b35517ec46a91ccfabf291bcb22
Yacine Bensidahmed <yacinebensidahmed.off@gmail.com>
2024-02-19 17:14:59 +0100

    this is the if statment Project

--- src/Main.java
+++ src/Main.java
@@ -2,56 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-//        comparison op (same as js)
-//        int x = 1;
-//        int y = 1;
-//        System.out.println(x == y);
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Number : ");
+        int nbr = scanner.nextInt();
 
-//        Logic op
-//        int temp = 10;
-//        boolean isWarm = temp > 20 && temp < 30;
-//        System.out.println(isWarm);
-
-//        boolean hasHightIncom = true;
-//        boolean hasGoodCredit = true;
-//        boolean hasCriminalRecord = false;
-//        boolean isEligible = (hasGoodCredit || hasHightIncom) && !hasCriminalRecord;
-//        System.out.println(isEligible);
-
-//        if statments
-//        if(temp > 30){
-//            System.out.println("It's a hot day \nDrink plenty of water");
-//        }
-//        else if(temp > 20)
-//            System.out.println("It's a nice day");
-//        else
-//            System.out.println("it's cold");
-
-//        Simplify if statement
-
-//        int income = 120_000;
-//        boolean hasHighIncome = (income > 100_000);
-//        System.out.println(hasHighIncome);
-
-//        Ternary operation
-//        String className = income > 100_000? "first" : "Economy";
-//        switch statement
-//        String role = "admin";
-//        switch (role){
-//            case "director":
-//                System.out.println("You are an director");
-//                break;
-//            case "admin":
-//                System.out.println("You are an admin");
-//                break;
-//            case "moderator":
-//                System.out.println("You are a moderator");
-//                break;
-//            default:
-//                System.out.println("you are a guest!");
-//        }
+        boolean divisible5 = (nbr % 5 == 0);
+        boolean divisible3 = (nbr % 3 == 0);
 
 
+
+        if(divisible3 && divisible5)
+            System.out.println("fizzBuzz");
+        else if(divisible5)
+            System.out.println("fizz");
+        else if(divisible3)
+            System.out.println("buzz");
+        else
+            System.out.println(nbr);
 
     }
 }
